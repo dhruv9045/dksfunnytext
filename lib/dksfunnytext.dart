998edf7b9f6c8c43e2a9b401f7869636ddd3026e
DksDev <singh1999dhruv@gmail.com>
2023-07-23 21:52:34 +0530

    23072023_update_comments

--- lib/dksfunnytext.dart
+++ lib/dksfunnytext.dart
@@ -4,23 +4,57 @@ import 'package:dksfunnytext/const_emojies.dart';
 import 'package:flutter/material.dart';
 
 class DksFunnyText extends StatefulWidget {
+  // Required text to be converted into emojis or displayed as simple text.
   final String text;
+
+  // Optional parameter to specify whether to replace some characters with emojis.
   final bool? replaceSome;
+
+  // Optional parameter to specify the locale for the text.
   final Locale? locale;
+
+  // Optional parameter to specify the maximum number of lines for the text.
   final int? maxLines;
+
+  // Optional parameter to specify the text overflow behavior.
   final TextOverflow? overflow;
-  final Color? selectionColor;
-  final String? semanticsLabel;
+
+  // Optional parameter to enable or disable soft wrapping of text.
   final bool? softWrap;
+
+  // Optional parameter to specify the color for text selection.
+  final Color? selectionColor;
+
+  // Optional parameter to provide a semantics label for accessibility.
+  final String? semanticsLabel;
+
+  // Optional parameter to specify the strut style for the text.
   final StrutStyle? strutStyle;
+
+  // Optional parameter to specify the text alignment.
   final TextAlign? textAlign;
+
+  // Optional parameter to specify the text direction.
   final TextDirection? textDirection;
+
+  // Optional parameter to specify the text height behavior.
   final TextHeightBehavior? textHeightBehavior;
+
+  // Optional parameter to specify the text scale factor.
   final double? textScaleFactor;
+
+  // Optional parameter to specify the text width basis.
   final TextWidthBasis? textWidthBasis;
+
+  // Optional parameter to specify the text style for the text.
   final TextStyle? style;
+
+  // Optional parameter to provide a custom emoji map to replace characters.
   final Map<String, dynamic>? emoji;
+
+  // Optional parameter to indicate whether to treat the text as simple text or not.
   final bool? simpleText;
+
   const DksFunnyText(
       {super.key,
       required this.text,
@@ -46,11 +80,14 @@ class DksFunnyText extends StatefulWidget {
 }
 
 class _DksFunnyTextState extends State<DksFunnyText> {
+  // Default emoji map containing characters and their corresponding emojis.
   Map<String, dynamic> defaultEmoji = emojis;
+  // List to store the results after converting text to emojis.
   List<String> results = [];
   @override
   void initState() {
     if (widget.simpleText == false) {
+      // Check if custom emojis are provided and update the default emoji map.
       if (widget.replaceSome == true && widget.emoji != null) {
         widget.emoji!.forEach((key, value) {
           if (defaultEmoji.containsKey(key)) {
@@ -58,8 +95,10 @@ class _DksFunnyTextState extends State<DksFunnyText> {
           }
         });
       } else {
+        // Use the provided custom emoji map or keep the default emoji map.
         widget.emoji != null ? defaultEmoji = widget.emoji! : null;
       }
+      // Split the text into words and convert each word into emojis.
       List<String> keys = widget.text.split(" ");
       for (String key in keys) {
         String result = '';
@@ -77,7 +116,9 @@ class _DksFunnyTextState extends State<DksFunnyText> {
 
   @override
   Widget build(BuildContext context) {
+    // Return the Text widget with the specified properties and converted text (if applicable).
     return Text(
+      // Apply the provided properties for the Text widget.
       locale: widget.locale,
       maxLines: widget.maxLines,
       overflow: widget.overflow,
@@ -91,8 +132,8 @@ class _DksFunnyTextState extends State<DksFunnyText> {
       textScaleFactor: widget.textScaleFactor,
       textWidthBasis: widget.textWidthBasis,
       widget.simpleText == true
-          ? widget.text
-          : results
+          ? widget.text // Display simple text if simpleText is true.
+          : results // Otherwise, display converted text with emojis.
               .toString()
               .replaceFirst('[', "")
               .replaceFirst(']', "")
